e897f977431dfd2de97ce2f994fc5f8fc682c1c1
Hai Nguyen <hai.nguyen.pv12@gmail.com>
2019-03-07 09:09:36 +0700

    rdy scripts for shop index

--- 1804HDTVShop/index.php
+++ 1804HDTVShop/index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>1804HDTV Flower Shop</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+
 <body>
-    
+
 </body>
+<!--Scripts placed at end of document to make pages load faster-->
+<script src="Scripts/angular.min.js"></script>
+<script src="Scripts/angular-route.js"></script>
+<script src="Scripts/jquery-3.3.1.min.js"></script>
+<script src="Scripts/umd/popper.min.js"></script>
+<script src="Scripts/bootstrap.min.js"></script>
+<!--Angular Controllers-->
+
 </html>
